04f09098754a17ac767ce5b8473f7ddf5addc40f
Ardizzone Pietro <ardizzone.pietro@osdbsesto>
2021-11-11 10:50:50 +0100

    calculation function

--- es03.js
+++ es03.js
@@ -4,29 +4,45 @@
 
 
     $("#btn1").click(function(){
-        
-      
+        calcolo("+");
+    });
+    var calcolo = function(segno){
         var  x = document.getElementById('num1').value;
         var y = document.getElementById('num2').value;
-     
         var table = document.getElementById('tab');
-        
+                            
         var row = table.insertRow();
         var cell1 = row.insertCell();
         var cell2 = row.insertCell();
         var cell3 = row.insertCell();
         var cell4 = row.insertCell();
         cell1.innerHTML = x;
-        cell2.innerHTML = "+";
+        
         cell3.innerHTML = y;
         var n1 = parseInt(x);
         var n2 = parseInt(y);
-
-        cell4.innerHTML = n1+n2;
+        switch(segno){
+        case "+":
+            cell4.innerHTML = n1+n2;
+            cell2.innerHTML = "+";
+        break;
+        case "-":
+            cell4.innerHTML = n1-n2;
+            cell2.innerHTML = "-";
+        break;
+        case "*":
+            cell4.innerHTML = n1*n2;
+            cell2.innerHTML = "*";
+        break;
+        case "/":
+            cell4.innerHTML = n1/n2;
+            cell2.innerHTML = "/";
+        break;
         
-
-       
-        });
+        }
+    } 
+                        
+        
         $("#btn2").click(function(){
             var  x = document.getElementById('num1').value;
         var y = document.getElementById('num2').value;
@@ -91,38 +107,37 @@
 
                     });
 
-                    var calcolo = function(segno){
-                        var  x = document.getElementById('num1').value;
-                        var y = document.getElementById('num2').value;
-                        var table = document.getElementById('tab');
-                                            
-                        var row = table.insertRow();
-                        var cell1 = row.insertCell();
-                        var cell2 = row.insertCell();
-                        var cell3 = row.insertCell();
-                        var cell4 = row.insertCell();
-                        cell1.innerHTML = x;
-                        cell2.innerHTML = "/";
-                        cell3.innerHTML = y;
-                        var n1 = parseInt(x);
-                        var n2 = parseInt(y);
-                        switch(segno){
-                        case "+":
-                            cell4.innerHTML = n1+n2;
-                        break;
-                        case "-":
-                            cell4.innerHTML = n1-n2;
-                        break;
-                        case "*":
-                            cell4.innerHTML = n1*n2;
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
  
-                        break;
-                        case "/":
-                            cell4.innerHTML = n1/n2;
-                        break;
-                        
-                        }
-                    } 
+                    
+                    
+                    
+                    
+                    
+                    
                     
                 
             
