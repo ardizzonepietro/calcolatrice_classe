d642451ff8c126ab05b00b147a81d78417e8dc27
Ardizzone Pietro <ardizzone.pietro@osdbsesto>
2021-11-11 10:42:41 +0100

    add function calc

--- es03.js
+++ es03.js
@@ -2,7 +2,10 @@
 
 
 
+
     $("#btn1").click(function(){
+        
+      
         var  x = document.getElementById('num1').value;
         var y = document.getElementById('num2').value;
      
@@ -85,10 +88,41 @@
                     var n2 = parseInt(y);
             
                     cell4.innerHTML = n1/n2;
-                    
-            
-               
+
                     });
+
+                    var calcolo = function(segno){
+                        var  x = document.getElementById('num1').value;
+                        var y = document.getElementById('num2').value;
+                        var table = document.getElementById('tab');
+                                            
+                        var row = table.insertRow();
+                        var cell1 = row.insertCell();
+                        var cell2 = row.insertCell();
+                        var cell3 = row.insertCell();
+                        var cell4 = row.insertCell();
+                        cell1.innerHTML = x;
+                        cell2.innerHTML = "/";
+                        cell3.innerHTML = y;
+                        var n1 = parseInt(x);
+                        var n2 = parseInt(y);
+                        switch(segno){
+                        case "+":
+                            cell4.innerHTML = n1+n2;
+                        break;
+                        case "-":
+                            cell4.innerHTML = n1-n2;
+                        break;
+                        case "*":
+                            cell4.innerHTML = n1*n2;
+ 
+                        break;
+                        case "/":
+                            cell4.innerHTML = n1/n2;
+                        break;
+                        
+                        }
+                    } 
                     
                 
             
