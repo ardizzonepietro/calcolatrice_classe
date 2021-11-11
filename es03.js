b53cb9f5fb22d46676b0b7df96974b5804702be2
Ardizzone Pietro <ardizzone.pietro@osdbsesto>
2021-11-11 11:42:02 +0100

    local storage

--- es03.js
+++ es03.js
@@ -1,11 +1,14 @@
 
 
-
+ var x =0;
 
 
     $("#btn1").click(function(){
         calcolo("+");
     });
+   
+   
+   
     var calcolo = function(segno){
         var  x = document.getElementById('num1').value;
         var y = document.getElementById('num2').value;
@@ -24,86 +27,45 @@
         switch(segno){
         case "+":
             cell4.innerHTML = n1+n2;
+            risultato = n1+n2;
+            var riga = n1 +"/"+segno+"/"+n2+"/"+risultato;
+           localStorage.setItem(x, riga)
+            localStorage.setItem('n1', n1);
+            localStorage.setItem('n2', n2);
+            localStorage.setItem('segno', "+");
+            localStorage.setItem('risultato', risultato);
             cell2.innerHTML = "+";
         break;
         case "-":
             cell4.innerHTML = n1-n2;
             cell2.innerHTML = "-";
+            localStorage.setItem('riga', n1, n2, segno, "-")
         break;
         case "*":
             cell4.innerHTML = n1*n2;
             cell2.innerHTML = "*";
+            localStorage.setItem('riga', n1, n2, segno, "*")
         break;
         case "/":
             cell4.innerHTML = n1/n2;
             cell2.innerHTML = "/";
+            localStorage.setItem('riga', n1, n2, segno, "/")
         break;
         
         }
+       
     } 
                         
         
         $("#btn2").click(function(){
-            var  x = document.getElementById('num1').value;
-        var y = document.getElementById('num2').value;
-     
-        var table = document.getElementById('tab');
-        
-        var row = table.insertRow();
-        var cell1 = row.insertCell();
-        var cell2 = row.insertCell();
-        var cell3 = row.insertCell();
-        var cell4 = row.insertCell();
-        cell1.innerHTML = x;
-        cell2.innerHTML = "-";
-        cell3.innerHTML = y;
-        var n1 = parseInt(x);
-        var n2 = parseInt(y);
-
-        cell4.innerHTML = n1-n2;
-        
-
-          
+          calcolo("-");
             });
             $("#btn3").click(function(){
-                var  x = document.getElementById('num1').value;
-        var y = document.getElementById('num2').value;
-     
-        var table = document.getElementById('tab');
-        
-        var row = table.insertRow();
-        var cell1 = row.insertCell();
-        var cell2 = row.insertCell();
-        var cell3 = row.insertCell();
-        var cell4 = row.insertCell();
-        cell1.innerHTML = x;
-        cell2.innerHTML = "*";
-        cell3.innerHTML = y;
-        var n1 = parseInt(x);
-        var n2 = parseInt(y);
-
-        cell4.innerHTML = n1*n2;
-        
+                calcolo("*");
 
                 });
-                $("#btn4").click(function(){
-                    var  x = document.getElementById('num1').value;
-                    var y = document.getElementById('num2').value;
-                 
-                    var table = document.getElementById('tab');
-                    
-                    var row = table.insertRow();
-                    var cell1 = row.insertCell();
-                    var cell2 = row.insertCell();
-                    var cell3 = row.insertCell();
-                    var cell4 = row.insertCell();
-                    cell1.innerHTML = x;
-                    cell2.innerHTML = "/";
-                    cell3.innerHTML = y;
-                    var n1 = parseInt(x);
-                    var n2 = parseInt(y);
-            
-                    cell4.innerHTML = n1/n2;
+                $("#btn4").click(function(){    
+                    calcolo("/");
 
                     });
 
